bd237855be5e5bb47ca98d32a9bafe629e4fb7fc
WoojinKim <dnwls488@naver.com>
2022-06-13 11:51:58 +0900

    SubQuery, Case 테스트

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -17,30 +17,47 @@ public class JpaMain {
         tx.begin();
 
         try{
-            /*
+
             Team team = new Team();
             team.setName("A");
             em.persist(team);
 
+            Team teamB = new Team();
+            teamB.setName("B");
+            em.persist(teamB);
+
+            Team teamC = new Team();
+            teamC.setName("C");
+            em.persist(teamC);
+
             Member member = new Member();
             member.setUsername("memberA");
             member.setAge(25);
-            member.setTeam(team);
-
+            member.changeTeam(team);
             em.persist(member);
 
             Member memberB = new Member();
             memberB.setUsername("memberB");
             memberB.setAge(23);
-            memberB.setTeam(team);
-
+            memberB.changeTeam(teamB);
             em.persist(memberB);
+
+            Member memberC = new Member();
+            memberC.setUsername("memberC");
+            memberC.setAge(8);
+            memberC.changeTeam(teamC);
+            em.persist(memberC);
+
+            Member memberD = new Member();
+            memberD.setUsername("memberD");
+            memberD.setAge(62);
+            memberD.changeTeam(teamC);
+            em.persist(memberD);
 
             Product product = new Product();
             product.setName("Sugar");
             product.setPrice(1000);
             product.setStockAmount(10);
-
             em.persist(product);
 
             Address address = new Address();
@@ -57,7 +74,6 @@ public class JpaMain {
 
             em.flush();
             em.clear();
-            */
             /**
              * TypedQuery - 반환 타입이 명확할 때
              * Query - 반환 타입이 명확하지 않을 때
@@ -135,6 +151,7 @@ public class JpaMain {
             /**
              * 페이징 쿼리
              */
+            /*
             for(int i=20; i<=60; i++) {
                 Member newMember = new Member();
                 newMember.setUsername("member" + i);
@@ -148,6 +165,50 @@ public class JpaMain {
                     .getResultList();
 
             resultList.stream().forEach(rs -> System.out.println("rs = " + rs));
+            */
+
+//            List<Member> resultList = em.createQuery("select m from Member m left join m.team t on t.name = 'B'", Member.class)
+//                    .getResultList();
+
+            /**
+             * Exists  서브쿼리에 결과가 존재하면 참
+             * ALL 모두 만족하면 참
+             * ANY, SOME : 조건을 하나라도 만족하면 참
+             * IN 서브 쿼리 결과 중 하나라도 같은 값이 있다면 참
+             */
+            /*
+            List<Member> resultList = em.createQuery("select m from Member m" +
+                            " where exists (select t from m.team t where t.name = 'B')", Member.class)
+                    .getResultList();
+
+            resultList.stream().forEach(rs -> System.out.println("rs = " + rs));
+             */
+
+            /**
+             * CASE
+             */
+            /*
+            List<String> resultList = em.createQuery(
+                            "select " +
+                                    "case when m.age <= 10 then '학생요금'" +
+                                    "     when m.age >= 60 then '경로요금'" +
+                                    "     else '일반요금' " +
+                                    "end " +
+                                    "FROM Member m", String.class)
+                    .getResultList();
+             */
+
+            List<String> resultList = em.createQuery(
+                            "select case t.name " +
+                                    "when 'A' then '인센티브110%'" +
+                                    "when 'B' then '인센티브120%'" +
+                                    "else '인센티브105%' " +
+                                    "END " +
+                                    "FROM Team t", String.class)
+                    .getResultList();
+
+            resultList.stream().forEach(rs -> System.out.println("rs = " + rs));
+
 
             tx.commit();
         } catch (Exception e) {
